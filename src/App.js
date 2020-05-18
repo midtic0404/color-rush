01684183467b5dcc18b7054f2e00b6fd047fb91a
Chia Che Chang <midtic0404@gmail.com>
2020-05-17 19:31:02 -0600

    Finished functionality to generate random colors

--- src/App.js
+++ src/App.js
@@ -1,70 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const generateColors = () => {
-    let red;
-    let green;
-    let blue;
-    let restColors;
+  const [normalColor, setNormalColor] = useState(null);
+  const [targetColor, setTargetColor] = useState(null);
 
+  const generateColors = () => {
+    let restColors = Math.floor(Math.random() * 96) + 80;
+    let red = restColors;
+    let green = restColors;
+    let blue = restColors;
+    let offset = Math.floor(Math.random() * 2) === 0 ? -30 : 30;
     let randRGB = Math.floor(Math.random() * 3);
 
     if (randRGB === 0) {
       red = 255;
+      setNormalColor(`rgb(${red}, ${blue}, ${green})`);
+      blue += offset;
+      green += offset;
     } else if (randRGB === 1) {
       green = 255;
+      setNormalColor(`rgb(${red}, ${blue}, ${green})`);
+      red += offset;
+      blue += offset;
     } else {
       blue = 255;
+      setNormalColor(`rgb(${red}, ${blue}, ${green})`);
+      red += offset;
+      green += offset;
     }
 
-    //Todo: Set up the rest of the color
+    setTargetColor(`rgb(${red}, ${blue}, ${green})`);
   };
 
   return (
     <div className='App'>
       <div className='board'>
         <div className='row'>
-          <div
-            className='circle'
-            style={{ background: 'rgb(255,200,200)' }}
-          ></div>
-          <div
-            className='circle'
-            style={{ background: 'rgb(255,220,220)' }}
-          ></div>
-          <div
-            className='circle'
-            style={{ background: 'rgb(255,220,220)' }}
-          ></div>
+          <div className='circle' style={{ background: normalColor }}></div>
+          <div className='circle' style={{ background: targetColor }}></div>
+          <div className='circle' style={{ background: normalColor }}></div>
         </div>
         <div className='row'>
-          <div
-            className='circle'
-            style={{ background: 'rgb(255,230,230)' }}
-          ></div>
-          <div
-            className='circle'
-            style={{ background: 'rgb(255,230,230)' }}
-          ></div>
-          <div
-            className='circle'
-            style={{ background: 'rgb(255,230,230)' }}
-          ></div>
+          <div className='circle' style={{ background: normalColor }}></div>
+          <div className='circle' style={{ background: normalColor }}></div>
+          <div className='circle' style={{ background: normalColor }}></div>
         </div>
         <div className='row'>
-          <div
-            className='circle'
-            style={{ background: 'rgb(255,230,230)' }}
-          ></div>
-          <div
-            className='circle'
-            style={{ background: 'rgb(255,230,230)' }}
-          ></div>
-          <div
-            className='circle'
-            style={{ background: 'rgb(255,230,230)' }}
-          ></div>
+          <div className='circle' style={{ background: normalColor }}></div>
+          <div className='circle' style={{ background: normalColor }}></div>
+          <div className='circle' style={{ background: normalColor }}></div>
         </div>
       </div>
       <button onClick={generateColors}>Get color</button>
